a2bb2a232ddd7be9ad9eba2628fd588c490596b3
Andreas Kull <akullpp@gmail.com>
2020-02-12 09:30:50 +0100

    Format and naming standards

--- rules/index.js
+++ rules/index.js
@@ -1,14 +1,14 @@
-'use strict';
+'use strict'
 
 module.exports = [
-	require('./heading'),
-	require('./badge'),
-	require('./contributing'),
-	require('./git-repo-age'),
-	require('./github'),
-	/// require('./license'),
-	require('./list-item'),
-	require('./no-ci-badge'),
-	require('./spell-check'),
-	require('./toc')
-];
+  require('./heading'),
+  require('./badge'),
+  require('./contributing'),
+  require('./git-repo-age'),
+  require('./github'),
+  /// require('./license'),
+  require('./list-item'),
+  require('./no-ci-badge'),
+  require('./spell-check'),
+  require('./toc'),
+]
